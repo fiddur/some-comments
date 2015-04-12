1987f8862cd0fb8c74f6bb9e6c9e638327526550
Fredrik Liljegren <fredrik.liljegren@textalk.se>
2015-04-12 12:59:42 +0200

    Making it somewhat usable.  Adding license.

--- index.js
+++ index.js
@@ -1,48 +1,76 @@
-var express = require('express')
-var q = require('q')
-global.app = express()
-var passport = require('passport')
-var config = require('./config.js')
-//var LocalStrategy = require('passport-local').Strategy
+/**
+ * Some Comments - a comment engine
+ * Copyright (C) 2015 Fredrik Liljegren
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt
+ * GNU-AGPL-3.0
+ */
+
+var express          = require('express')
+var q                = require('q')
+var passport         = require('passport')
+var bodyParser       = require('body-parser')
+var config           = require('./config.js')
 var FacebookStrategy = require('passport-facebook').Strategy
-var GithubStrategy = require('passport-github').Strategy
-var GoogleStrategy = require('passport-google').Strategy
-//var morgan = require('morgan')
-var cookieSession = require('cookie-session')
+var GithubStrategy   = require('passport-github').Strategy
+var GoogleStrategy   = require('passport-google').Strategy
+var cookieSession    = require('cookie-session')
+var cors             = require('cors')
 
-var users = {
-  fiddur: { username: 'fiddur', password: 'secret', email: 'fredrik@liljegren.org' }
-}
+global.app = express()
 
-function User(id, displayName) {
+/**
+ * User
+ */
+function User(id, displayName, avatar) {
   this.id          = id
   this.displayName = displayName
+  this.avatar      = avatar
 }
 User.getById = function(id) {
-  var deferred = q.defer()
+  return global.app.locals.db
+    .get('SELECT * FROM users WHERE id = ?', id)
+    .then(function(userData) {
+      if (typeof userData === 'undefined') {
+        throw 'No user with id: ' + id
+      }
 
-  global.app.locals.db.get('SELECT * FROM users WHERE id = ?', id, function(err, userData) {
-    if (typeof userData === 'undefined') {
-      deferred.reject('No such user')
-    }
-    else {
-      var user = new User(id, userData.displayName)
-      deferred.resolve(user)
-    }
-  })
-
-  return deferred.promise
+      return new User(id, userData.displayName, userData.avatar)
+    }, function(error) {
+      console.log(error)
+    })
 }
-User.create = function(displayName) {
-  var deferred = q.defer()
-
-  global.app.locals.db.run('INSERT INTO users (displayName) VALUES(?)', displayName, function(err) {
-    deferred.resolve(new User(this.lastID, displayName))
-  })
-
-  return deferred.promise
+User.create = function(displayName, avatar) {
+  return global.app.locals.db
+    .run('INSERT INTO users (displayName, avatar) VALUES(?,?)', displayName, avatar)
+    .then(function(db) {
+      return new User(db.lastID, displayName, avatar)
+    })
+}
+User.prototype.toString = function() {
+  return this.displayName + ' (' + this.id + ')'
 }
 
+/**
+ * Account - representing a login account at a connected service like Google, Facebook, Openid etc.
+ *
+ * @param uid     The unique identifier on the connected system
+ * @param system  The authentication system - normally the passport strategy used.
+ * @param user    The User ID.
+ */
 function Account(id, uid, system, user) {
   this.id     = id
   this.uid    = uid
@@ -50,29 +78,97 @@ function Account(id, uid, system, user) {
   this.user   = user
 }
 Account.getOrCreate = function(system, uid, displayName) {
-  var deferred = q.defer()
-
-  global.app.locals.db.get(
-    'SELECT * FROM accounts WHERE system = ? AND uid = ?',
-    system, uid, function(err, account_data) {
+  return global.app.locals.db
+    .get('SELECT * FROM accounts WHERE system = ? AND uid = ?', system, uid)
+    .then(function(account_data) {
       if (typeof account_data === 'undefined') {
-        return User.create(displayName)
+        return User.create(displayName, 'http://graph.facebook.com/' + uid + '/picture')
           .then(function(user) {
-            global.app.locals.db.run(
-              'INSERT INTO accounts (uid, system, user) VALUES (?,?,?)', uid, system, user.id,
-              function(err) {
-                deferred.resolve(new Account(this.lastID, uid, system, user.id))
-              }
+            return global.app.locals.db.run(
+              'INSERT INTO accounts (uid, system, user) VALUES (?,?,?)', uid, system, user.id
             )
+          }).then(function(db) {
+            return new Account(db.lastID, uid, system, user.id)
           })
       }
       else {
-        deferred.resolve(new Account(account_data.id, uid, system, account_data.user))
+        return new Account(account_data.id, uid, system, account_data.user)
       }
     })
-
-  return deferred.promise
 }
+
+/**
+ * Site - a site to comment things on.
+ */
+function Site(id, domain) {
+  this.id     = id
+  this.domain = domain
+}
+Site.add = function(domain) {
+  return global.app.locals.db
+    .run('INSERT INTO sites (domain) VALUES (?)', domain)
+    .then(function(db) {
+      console.log('Created Site', db.lastID, domain)
+      return new Site(db.lastID, domain)
+    })
+}
+Site.prototype.addAdmin = function(user) {
+  return global.app.locals.db
+    .run('INSERT INTO siteadmins (site, user) VALUES (?,?)', this.id, user.id)
+}
+
+/**
+ * Comment
+ */
+function Comment(id, text, user, page, parent, deleted) {
+  this.id       = id
+  this.text     = text
+  this.user     = user
+  this.page     = page
+  this.parent   = parent
+  this.deleted  = deleted
+  this.children = null
+}
+Comment.add = function(site, page, user, text, parent) {
+  console.log('Adding a comment for ' + site + ' on ' + page + ' by ' + user + ': ' + text)
+
+  return global.app.locals.db
+    .run('INSERT INTO comments (text, user, site, page, parent) VALUES (?,?,?,?,?)',
+         text, user, site, page, parent)
+}
+Comment.getAllByPage = function(site, page) {
+  console.log('Getting comments for site ' + site + ' and page ' + page)
+
+  return global.app.locals.db
+    .all('SELECT comments.id, comments.parent, comments.text, comments.user, users.displayName, users.avatar FROM comments LEFT JOIN users ON users.id = comments.user WHERE site=? AND page=? AND deleted IS NULL ORDER BY comments.id',
+         site, page)
+    .then(function(commentRows) {
+      console.log(commentRows)
+
+      var commentById = {}
+      var usersToGet = []
+      var comments = [] // Comments without parent
+
+      for (var i = 0; i < commentRows.length; i++) {
+        var comment = commentRows[i]
+
+        comment.children = [] // We promise to fill in children for all comments.
+        commentById[comment.id] = comment
+
+        if (comment.parent === null) {comments.push(comment)}
+        else {
+          if (typeof commentById[comment.parent] === 'undefined') {
+            throw 'Comment ' + comment.id + ' claims to have a parent that isn\'t loaded.'
+          }
+
+          commentById[comment.parent].children.push(comment)
+        }
+      }
+
+      return comments
+    })
+}
+
 
 // serialize and deserialize
 passport.serializeUser(function(user, done) {
@@ -80,6 +176,7 @@ passport.serializeUser(function(user, done) {
   done(null, user.id)
 })
 passport.deserializeUser(function(id, done) {
+  console.log('Deserializing user 1')
   User.getById(id)
     .then(function(user) {
       console.log("Deserialized user:", user)
@@ -119,13 +216,21 @@ var expressHbs = require('express3-handlebars')
 app.engine('hbs', expressHbs({extname: 'hbs', defaultLayout: 'main.hbs'}))
 app.set('view engine', 'hbs')
 
-app.use(cookieSession({keys: ['ett', 'två']}))
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
+app.use(cookieSession({httpOnly: false, keys: ['ett', 'två']}))
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(express.static(__dirname + '/public'))
+app.use(function(req, res, next) {
+  console.log('http://' + req.headers.host + req.url, req.user)
+  next()
+})
+
+app.use(cors({credentials: true, origin: true}))
 
 // routes
 app.get('/', function(req, res) {res.render('index')})
+app.get('/login', function(req, res) {res.render('login')})
 
 app.get('/ping', function(req, res) {res.send('pong')})
 
@@ -165,19 +270,104 @@ function ensureAuthenticated(req, res, next) {
 }
 
 
-var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database(config.database.connection.filename)
-app.locals.db = db
+var sqlite3  = require('sqlite3').verbose()
+var qsqlite3 = require('q-sqlite3')
+
+qsqlite3.createDatabase(config.database.connection.filename)
+  .done(function(db) {
+    app.locals.db = db
+    setup_db(db)
+  })
 
 function setup_db(db) {
   // Setup tables
-  db.run('CREATE TABLE users (id INTEGER PRIMARY KEY, displayName STRING)')
+  db.run('CREATE TABLE users (id INTEGER PRIMARY KEY, displayName STRING, avatar STRING)')
   db.run('CREATE TABLE accounts (id INTEGER PRIMARY KEY, uid STRING, system STRING, user INTEGER)')
   db.run('CREATE TABLE sites (id INTEGER PRIMARY KEY, domain STRING, maxLevels INTEGER)')
   db.run('CREATE TABLE siteadmins (site INTEGER, user INTEGER)')
-  db.run('CREATE TABLE comments (id INTEGER PRIMARY KEY, text TEXT, user INTEGER, page STRING, parent INTEGER, deleted DATETIME)')
+  db.run('CREATE TABLE comments (id INTEGER PRIMARY KEY, text TEXT, user INTEGER, site INTEGER, page STRING, parent INTEGER, created DATETIME DEFAULT CURRENT_TIMESTAMP, changed DATETIME, deleted DATETIME)')
   db.run('CREATE TABLE subscription (page STRING, user INTEGER)')
   db.run('CREATE TABLE settings (key STRING, value STRING)')
   db.run('CREATE TABLE superadmins (user INTEGER)')
+  db.run('CREATE UNIQUE INDEX IF NOT EXISTS domain ON sites (domain)')
+  db.run('CREATE UNIQUE INDEX IF NOT EXISTS system_uid ON accounts (system, uid)')
+  db.run('CREATE UNIQUE INDEX IF NOT EXISTS siteuser ON siteadmins (site, user)')
 }
-//setup_db(db)
+
+
+
+/**
+ * REST API for /sites/
+ */
+
+/**
+ * GET /sites/ to list all sites.
+ */
+app.get('/sites/', function(req, res) {
+  var db = req.app.locals.db
+
+  db.all('SELECT * FROM sites ORDER BY id')
+    .then(function(sites_data) {
+      res.json(sites_data)
+    })
+})
+
+/**
+ * POST to /sites/ to create a new one.
+ */
+app.post('/sites/', function(req, res) {
+  if (typeof req.body.domain === 'undefined') {
+    return res.status(400).send('Bad Request: domain is required')
+  }
+
+  if (typeof req.user === 'undefined') {
+    console.log('Unauthorized', req.headers.host, req.url)
+    return res.status(401).send('Unauthorized')
+  }
+
+  console.log(req.user)
+
+  Site.add(req.body.domain)
+    .then(function(site) {
+      site.addAdmin(req.user) // No need to wait for it to finish.
+
+      res
+        .status(201)
+        .location('/sites/' + site.id)
+        .send('Created site ' + site.id + ' with admin ' + req.user)
+    }, function(error) {
+      res
+        .status(500)
+        .send(error)
+    })
+})
+
+//app.use(function(err, req, res, next) {
+//  console.error(err.stack)
+//  res.status(500).send('Something broke!', err)
+//})
+
+app.get('/sites/:site/pages/:page/comments/', function(req, res) {
+  console.log('Getting comments')
+  Comment.getAllByPage(req.params.site, req.params.page)
+    .then(function(comments) {
+      console.log('Sending comments')
+      res.send(comments)
+    }, function(error) {console.log(error); res.status(500).send(error)})
+})
+
+app.post('/sites/:site/pages/:page/comments/', function(req, res) {
+  if (typeof req.user === 'undefined') {return res.status(401).send('Unauthorized')}
+
+  if (typeof req.body.text === 'undefined') {
+    return res.status(400).send('Bad Request: text is required')
+  }
+
+  Comment.add(req.params.site, req.params.page, req.user.id, req.body.text, null)
+    .then(function(comment) {
+      console.log('Comment added', comment)
+      res.status(201).location(req.path + comment.id).send(comment)
+    }, function(error) {res.status(500).send(error)})
+})
+
+// @license-end
---
+++ public/client.js
@@ -0,0 +1,167 @@
+/**
+ * Some Comments - a comment engine
+ * Copyright (C) 2015 Fredrik Liljegren
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt
+ * GNU-AGPL-3.0
+ */
+
+function(window) {
+  /**
+   * Make a shortcut to document.getElementById…
+   */
+  var e = function (id) {return document.getElementById(id)}
+
+  /**
+   * Minimal ajax wrapper with promises.
+   */
+  var ajax = {}
+  ajax.call = function(method, url, headers, body) {
+    var deferred = Q.defer()
+    var req = new XMLHttpRequest()
+    req.withCredentials = true
+    req.open(method, url, true)
+
+    for (var header in headers) {req.setRequestHeader(header, headers[header])}
+
+    req.onload = function() {
+      if (req.status == 200) {return deferred.resolve(req.response)}
+      if (req.status == 401) {
+        return User.offerLogin(method, url, headers, body)
+      }
+      deferred.reject(req.statusText)
+    }
+    req.onerror = function() {deferred.reject("Network failure")}
+    req.send(body)
+
+    return deferred.promise
+  }
+  ajax.get = function(url) {
+    return ajax.call('GET', url, {}, '')
+  }
+  ajax.post = function(url, data) {
+    headers = {}
+    headers['content-type'] = 'application/json'
+    body = JSON.stringify(data)
+    return ajax.call('POST', url, headers, body)
+  }
+
+  var User = {}
+  /**
+   * Display a login iframe, promise to fulfil the original request.
+   */
+  User.offerLogin = function(method, url, headers, body) {
+    var iframe = document.createElement('iframe')
+    iframe.src = "http://localhost:1337/login"
+    iframe.className = 'login'
+
+    var deferred = Q.defer()
+    window.addEventListener("message", function(event) {
+      if (event.origin !== 'http://localhost:1337') {
+        console.log('This does not originate from localhost:1337! Ignoring')
+        return
+      }
+
+      if (!event.data.authenticated) {return deferred.reject('Not authenticated')}
+
+      // Resend ajax request.
+      document.body.removeChild(iframe)
+      ajax.call(method, url, headers, body).then(deferred.resolve)
+    }, false);
+
+    document.body.appendChild(iframe)
+    console.log(iframe.contentWindow)
+
+    return deferred.promise
+  }
+
+  var Site = {}
+  Site.add = function(domain) {
+    return ajax.post(
+      'http://localhost:1337/sites/', {domain: domain})
+      .then(
+        function(response) {
+          console.log('Added site', response)
+        }, function(error) {
+          console.log('Error', error)
+        }
+      )
+  }
+  Site.list = function() {
+    return ajax.get('http://localhost:1337/sites/')
+      .then(function(sites_json) {
+        console.log('in list')
+        return JSON.parse(sites_json)
+      })
+  }
+
+  Site.list().then(
+    function(sites) {
+      console.log("Number of sites", sites.length)
+      e('sites').innerHTML = sites
+    },
+    function(error) {console.log('Error', error)}
+  )
+
+
+  var Comment = {}
+
+  Comment.getAllByPage = function(site, page) {
+    return ajax.get('http://localhost:1337/sites/' + site + '/pages/' + page + '/comments/')
+      .then(function(commentsJson) {
+        return JSON.parse(commentsJson)
+      })
+  }
+
+  Comment.add = function(site, page, text) {
+    return ajax.post(
+      'http://localhost:1337/sites/' + site + '/pages/' + page + '/comments/', {text: text})
+      .then(
+        function(response) {
+          console.log('Added comment', response)
+        }, function(error) {
+          console.log('Error', error)
+        }
+      )
+  }
+
+  Comment.displayAll = function(comments, element) {
+    console.log(element, comments)
+    for (var i = 0; i < comments.length; i++) {
+      element.appendChild(Comment.getElement(comments[i]))
+    }
+  }
+
+  Comment.getElement = function(comment) {
+    var div = document.createElement('div')
+
+    div.innerHTML =
+      '<div class="user"><img alt="' + comment.displayName + '" src="' + comment.avatar + '" /></div>'
+      + '<div class="comment_text">'
+      + markdown.toHTML('**' + comment.displayName + '**: ' + comment.text) + '</div>'
+    console.log(div)
+
+
+    return div
+  }
+
+  // Make some things available on window
+  window.Comment = Comment
+  window.Site    = Site
+  window.User    = User
+}(window)
+
+// @license-end
