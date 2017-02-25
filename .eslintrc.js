ec3232214a788f701e0c38595ba6d0886b618a92
Fredrik Liljegren <fredrik@liljegren.org>
2017-02-25 09:08:04 +0100

    Adding some style

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  'extends': 'airbnb',
+  'installedESLint': true,
+  'plugins': [
+    'jsx-a11y',
+  ],
+  'rules': {
+    'semi': ['error', 'never'],
+    'no-multi-spaces': 0,
+    'key-spacing': ['error', {'align': 'value'}],
+    'new-cap': 0,
+    'no-nested-ternary': 0,
+    'no-console': 0,
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'arrow-parens': ['error', 'as-needed'],
+    'comma-dangle': ['error', {
+        arrays:    'always',
+        objects:   'always',
+        imports:   'always',
+        exports:   'always',
+        functions: 'ignore',
+    }]
+
+  },
+}
